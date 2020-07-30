b2b4df223f88372ccfe224ac4ac9ae3b6ce337cf
yskim621 <yskim62100@gmail.com>
2020-07-30 16:16:51 +0900

    ListView 출력 성공 2020.07.30

--- app/src/main/java/com/example/androidportfolio/MainActivity.java
+++ app/src/main/java/com/example/androidportfolio/MainActivity.java
@@ -2,18 +2,29 @@ package com.example.androidportfolio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.LayoutAnimationController;
+import android.view.animation.TranslateAnimation;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -26,6 +37,11 @@ import java.util.List;
 import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
+    ProgressBar ind;
+
+    // 스크롤이 가장 아래에서 수행되었는지 확인할 변수
+    boolean lastItemVisibleFlag = false;
+
     //콤보 박스 역할을 하는 위젯
     private Spinner searchtype;
     private EditText value;
@@ -59,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
 
             // adapter를 이용해서 Listview에 데이터가 수정되면 다시 출력하도록 [신호를 보냄 - notification]
             itemAdapter.notifyDataSetChanged();
+            ind.setVisibility(View.INVISIBLE);
         }
     };
 
@@ -135,6 +152,9 @@ public class MainActivity extends AppCompatActivity {
                     item.pictureurl = temp.getString(4);
 
                     list.add(item);
+                    // 이메일과 같이 최신 데이터를 위에서 붙여서 이전 데이터를 밑으로 누르는 방식으로 데이터를 추가하기 위해서는
+                    // index 0에 계속 추가를 해서 이전 데이터의 인덱스를 밀어냄
+                    // list.add(0, item);
                 }
                 //핸들러에게 출력을 요청
                 handler.sendEmptyMessage(0);
@@ -148,6 +168,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ind = (ProgressBar) findViewById(R.id.progressind);
+
 
         searchtype = (Spinner)findViewById(R.id.searchtype);
         adapter = ArrayAdapter.createFromResource(
@@ -166,6 +189,77 @@ public class MainActivity extends AppCompatActivity {
 
         listView.setAdapter(itemAdapter);
 
+        // 옵션 설정
+        listView.setDivider(new ColorDrawable(Color.BLUE));
+        listView.setDividerHeight(3);
+
+        // 항목을 선택했을 때 호출되는 이벤트 핸들러 작성
+        listView.setOnItemClickListener(new ListView.OnItemClickListener(){
+            @Override
+            //adapterView는 이벤트가 발생한 뷰, view는 선택한 항목 뷰, i가 선택한 항목의 인덱스, l은 선택한 항목 뷰의 id
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //선택한 항목의 데이터
+                Item item = list.get(i);
+                //토스트로 itemid를 출력
+                Toast.makeText(MainActivity.this, item.itemid + "", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // ListView의 항목 animation 설정
+        AnimationSet set = new AnimationSet(true);
+
+        // 이동하는 애니메이션 -> 짝수 매개변수는 좌표애 관한 매개변수
+        Animation rtl = new TranslateAnimation(
+                Animation.RELATIVE_TO_SELF, 1.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f
+        );
+        rtl.setDuration(3000);
+        set.addAnimation(rtl);
+
+        Animation alpha = new AlphaAnimation(0.0f, 1.0f);
+        alpha.setDuration(3000);
+        set.addAnimation(alpha);
+
+        // 각각의 애니메이션을 설정하고 대기 시작을 추가해서 설정
+        LayoutAnimationController controller = new LayoutAnimationController(set, 0.5f);
+
+        // ListView에 애니메이션을 설정
+        listView.setLayoutAnimation(controller);
+
+
+        // listView의 Scroll 이벤트 처리
+        listView.setOnScrollListener(new ListView.OnScrollListener() {
+            // Scroll이 끝나면 호출되는 메소드
+            @Override
+            // 첫번째 매개변수는 스크롤이 발생한 뷰
+            // 두번째는 현재 스크롤 상태로 OnScrollListener의 상수로 설정
+            public void onScrollStateChanged(AbsListView absListView, int scrollState) {
+                // 스크롤이 끝나고 가장 하단에서 스크롤을 했다면 데이터 업데이트
+                if(scrollState == ListView.OnScrollListener.SCROLL_STATE_IDLE && lastItemVisibleFlag == true){
+                    pageNo = pageNo + 1;
+                    if((pageNo-1) * 3 +1 >= cnt){
+                        Toast.makeText(MainActivity.this, "더 이상 데이터가 존재하지 않습니다.", Toast.LENGTH_LONG).show();
+                    } else {
+                        new ThreadEx().start();
+                    }
+                }
+            }
+
+            // Scroll 도중에 호출되는 메소드
+            @Override
+            // 두번째 매개변수 - 첫번째 보이는 아이템의 인덱스
+            // 세번째 매개변수 - 현재 보여지고 있는 행의 개수
+            // 네번째 매개변수 - 전체 출력된 행의 개수
+            public void onScroll(AbsListView absListView, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                // 스크롤 하는 위치를 감시하다가 마지막인지 인지해서 값을 변경
+                if(totalItemCount > 0 && firstVisibleItem + visibleItemCount >= totalItemCount){
+                    lastItemVisibleFlag = true;
+                }
+
+            }
+        });
 
         btnnext.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
@@ -177,7 +271,10 @@ public class MainActivity extends AppCompatActivity {
         btnsearch.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
                 pageNo = 1;
-                result = "";
+                //result = "";
+
+                // List를 초기화해서 ListView를 초기화
+                list.clear();
                 new ThreadEx().start();
             }
         });
@@ -188,5 +285,6 @@ public class MainActivity extends AppCompatActivity {
     public void onResume(){
         super.onResume();
         new ThreadEx().start();
+        ind.setVisibility(View.VISIBLE);
     }
 }
